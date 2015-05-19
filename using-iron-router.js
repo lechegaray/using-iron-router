1b183933d7913df96c309d43c307dad45f57c996
Luis Echegaray <mrluisechegaray@gmail.com>
2015-05-19 16:31:08 -0400

    added more templates

--- using-iron-router.js
+++ using-iron-router.js
@@ -1,18 +1,55 @@
-Router.route('/', function () {
-  var isLoggedIn = Session.get('isLoggedIn');
+Articles = new Meteor.Collection('articles');
 
-  this.layout('AppLayout');
-
-  if (isLoggedIn)
-    this.render('Home');
-  else
-    this.render('Login');
+Router.configure({
+  layoutTemplate: 'Layout',
+  loadingTemplate: 'Loading'
 });
 
-if (Meteor.isClient) {
+Router.plugin('authorize', {
+  only: ['article.new'],
+  notAuthorizedRoute: 'home'
+});
 
-}
+Router.use(Router.bodyParser.json());
+
+Router.route('/', {name: 'home'});
+Router.route('/blog/new', {name: 'article.new'});
+Router.route('/blog/:_id', {name: 'article.show'});
+
+Router.route('/webhooks/stripe', {name: 'webhooks.stripe', where: 'server'})
+  .get('get')
+  .post('post');
 
 if (Meteor.isServer) {
+  var Future = Npm.require('fibers/future');
 
+  Meteor.publish('articles', function () {
+    var future = new Future;
+
+    // simulate a 5 second delay before the subscription
+    // is "ready"
+    setTimeout(Meteor.bindEnvironment(function () {
+      future.return(Articles.find());
+    }), 5000);
+
+    return future.wait();
+  });
+
+  Meteor.publish('article', function (id) {
+    return Articles.find({_id: id});
+  });
+
+  Meteor.startup(function () {
+    if (Articles.find().count() > 0)
+      return;
+
+    for (var i = 0; i < 3; i++) {
+      Articles.insert({
+        title: 'Blog Article ' + i,
+        body: 'This is the text body for the article I want to show.',
+        createdAt: new Date,
+        author: 'Chris Mather'
+      });
+    }
+  });
 }
